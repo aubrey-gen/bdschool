3063ff00e7bcec3463652e854b18f402eef5b1fd
Aubrey <breowas@gmail.com>
2020-01-08 16:33:56 +0200

    Delete DB_CONNECT.php

--- includes/php/DB_CONNECT.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /*  Database connection using PDO   */
-    $host = "localhost";
-    $dbn = "st_users";
-    $usn = "aubrey";       
-    $pword = "mota3t";
-    /*  Connect to database */
-    $link = new PDO("mysql:host=$host;dbname=$dbn", $usn, $pword);
-?>
